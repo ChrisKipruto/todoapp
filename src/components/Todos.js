ed52388b94f4922ab3f2219d38d32f16fc79a51b
chris <kiprutochris7@gmail.com>
2020-05-21 09:24:38 +0300

    completed tailwindcss config and styling

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,9 +6,7 @@ const Todos = ({todos, deleteTodo}) => {
             return (
                 <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
                     {todo.content}
-                    <span onClick={() => {deleteTodo(todo.id)}} className="badge badge-danger badge-pill cursor-pointer">
-                        <i className="fas fa-trash"></i>
-                    </span>
+                    <i onClick={() => {deleteTodo(todo.id)}} className="fas fa-trash red-text shadow-md cursor-pointer"></i>
                 </li>
             )
         })
@@ -19,7 +17,7 @@ const Todos = ({todos, deleteTodo}) => {
     return (
         <div className="todos row d-flex justify-content-center px-4 py-4 mt-5">
             <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
-                <h1 className="pink-text text-center font-weight-bold mb-4">Todo's</h1>
+                <h1 className="pink-text text-center font-weight-bold mb-4 text-6xl">Todo's</h1>
                 <ul className="list-group">
                     {todoList}
                 </ul>
